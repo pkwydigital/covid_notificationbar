cb445e53fcf514230fd118c5fb062c68b5dca104
Robert Klingensmith <robk@satellite2018.lan>
2020-03-13 21:50:07 -0400

    Version 1.0.2

--- covid_notificationbar.php
+++ covid_notificationbar.php
@@ -3,7 +3,7 @@
  * Plugin Name: COVID-19 Notification Bar
  * Plugin URI: https://www.pkwydigital.com
  * Description: Notification Bar for COVID-19 Coronavirus
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: Parkway Digital
  * Author URI: http://www.pkwydigital.com
  */
